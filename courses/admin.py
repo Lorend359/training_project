c74130e8be45c0d59c667c20aeb9160221ae68f9
Svyatoslav <arend359@gmail.com>
2025-07-01 23:28:20 +0300

    Register models in Django admin for users, courses, and assessments

--- courses/admin.py
+++ courses/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Course, Lesson
 
-# Register your models here.
+
+@admin.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ("title", "owner", "created_at")
+    search_fields = ("title", "owner__email")
+    list_filter = ("created_at",)
+
+
+@admin.register(Lesson)
+class LessonAdmin(admin.ModelAdmin):
+    list_display = ("title", "course", "order")
+    search_fields = ("title", "course__title")
+    list_filter = ("course",)
